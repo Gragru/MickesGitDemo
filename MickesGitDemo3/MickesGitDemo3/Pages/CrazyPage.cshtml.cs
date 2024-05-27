46e7b457f4ea5fb7f830b2b07908617cda637922
Mikael Engström <micke@primat.se>
2024-05-27 11:50:01 +0200

    Added Crazy Page

---
+++ MickesGitDemo3/MickesGitDemo3/Pages/CrazyPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MickesGitDemo3.Pages
+{
+    public class CrazyPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
